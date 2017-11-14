bdce997d41c635c0af5adff2eac977fb6895e567
Prashant Karmankar <kmkpbk@gmail.com>
2017-11-15 00:23:47 +0530

    added cross origin

--- src/main/java/com/amdocs/scanandpay/controllers/IndexController.java
+++ src/main/java/com/amdocs/scanandpay/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.amdocs.scanandpay.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.amdocs.scanandpay.models.Product;
 import com.amdocs.scanandpay.repository.IProductRepository;
 
+@CrossOrigin
 @RestController
 public class IndexController {
 
