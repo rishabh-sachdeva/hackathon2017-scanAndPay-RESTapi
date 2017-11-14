eca2cbf5aca477aa37e7d4087bc71577e786a79d
Prashant Karmankar <kmkpbk@gmail.com>
2017-11-14 07:27:47 +0200

    Initial Commit

---
+++ src/main/java/com/amdocs/scanandpay/ScanAndPayRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.amdocs.scanandpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanAndPayRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanAndPayRestApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/amdocs/scanandpay/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.amdocs.scanandpay.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@GetMapping("/")
+	public String get() {
+		return "Hello";
+	}
+}
